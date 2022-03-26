b981218366f1effb2d64dbb2088b30eb518e5708
maxo <maximino33@hotmail.com>
2022-03-26 13:07:12 -0600

    basic struct of menu added

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import './App.css';
+import {Mobile_menu} from './components/Mobile_menu'
+
+import image from './assets/icons/hamburger_menu.svg'
 
 function App() {
   return (
     <div className="App">
-      
+      <Mobile_menu img={image}/>
     </div>
   );
 }
