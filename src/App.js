78ce68f54a5dde075231070cbe21cd0787fcad78
maxo <maximino33@hotmail.com>
2022-03-26 17:29:13 -0600

    Home component added to App.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
-import {Mobile_menu} from './components/Mobile_menu'
-
 import image from './assets/icons/hamburger_menu.svg'
+
+// COMPONENTS
+import {Home} from './pages/Home'
 
 function App() {
   return (
     <div className="App">
-      <Mobile_menu img={image} menuList={['Home', 'About me', 'My work', 'Experience', 'Education']}/>
+      <Home/>
     </div>
   );
 }
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,12 @@
 import React from "react";
+import {Layout} from "../containers/Layout"
 
 export const Home = ()=>{
     return(
-        <div className="">
-            
+        <div className="Home">
+            <Layout>
+
+            </Layout>
         </div>
     )
 }
