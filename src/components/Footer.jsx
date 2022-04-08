fd8c7d0f844654838975d83493532fe6c4b37d50
maxo <maximino33@hotmail.com>
2022-04-08 00:59:35 -0500

    details header btn added

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,7 +4,9 @@ export const Footer = () => {
     return (
         <div className="Footer border-t border-light bg-dark flex items-center p-5 justify-around">
             <p className="text-white text-xl">Emmanuel Lopez Portafolio 2022</p>
-            <button className="border border-white rounded px-3 py-2 shadow-bl-white text-xl  block text-white">
+            <button className="border border-white rounded px-3 py-2 
+            shadow-bl-white text-xl block text-white 
+            hover:bg-primary hover:border-primary transition-all duration-500">
                 Descargar cv
             </button>
         </div>
